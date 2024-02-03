cf550ea5d763c37b9184e46510f48136666696c9
s0ar <20152463+s-0-a-r@users.noreply.github.com>
2024-02-03 18:39:05 +0900

    feat: add items tables (#6)
    * feat: add items tables
    
    * fix: rename
    
    - database -> db

--- src/db/migrations/1705239811141-create-users.ts
+++ src/db/migrations/1705239811141-create-users.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUserTable1705239811141 implements MigrationInterface {
-    name = 'CreateUserTable1705239811141'
+export class CreateUsers1705239811141 implements MigrationInterface {
+    name = 'CreateUsers1705239811141'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`users\` (\`id\` varchar(36) NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
